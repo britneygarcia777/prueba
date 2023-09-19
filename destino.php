9e5190366b483d285eea524b5516ba983c0f7adf
unknown <@gmail.com>
2023-09-19 14:46:36 -0400

    pimer commit

---
+++ destino.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Datos recividos</h1>
+	<?php
+	echo $_POST['email'];
+	echo "<br>";
+	echo  $_POST['nombre'];
+	echo "<br>";
+	echo  $_POST['password'];
+	echo "<br>";
+	echo  $_POST['nombrepj'];
+	echo "<br>";
+	echo $_POST['ciudad'];
+	?>
+</body>
+</html>
